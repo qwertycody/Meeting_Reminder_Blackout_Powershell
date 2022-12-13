019976ac04dff032f55af0077224c0eceaa645bf
Cody Garrett <cody@garrett.dev>
2022-12-12 20:30:05 -0500

    Change behavior to kill open processes before creating new ones, use different subprocess method

--- execute.ps1
+++ execute.ps1
@@ -1,4 +1,19 @@
-$textToDisplay = $args
+$textToDisplay = $args[0]
+
+# Begin - Kill Existing Processes
+$powershellProcesses = Get-WmiObject Win32_Process | Where-Object {$_.Name -like "*Powershell*"}
+
+foreach($powershellProcess in $powershellProcesses)
+{
+    $commandLine = $powershellProcess.CommandLine
+
+    if($commandLine -like "*createReminder*")
+    {
+        taskkill /F /PID $powershellProcess.ProcessId
+    }
+}
+
+# End - Kill Existing Processes
 
 [reflection.assembly]::loadwithpartialname('System.Windows.Forms');
 
@@ -7,16 +22,8 @@ $scriptPath = split-path -parent $MyInvocation.MyCommand.Definition
 $screens = [System.Windows.Forms.Screen]::AllScreens
 $screenCount = $screens.Count
 
-$jobList = New-Object System.Collections.ArrayList
-
 for($screenIndex = 0; $screenIndex -lt $screenCount; $screenIndex++)
 {
     Write-Output "Creating Reminder Window with $textToDisplay on Screen $screenIndex"
-    $job = Start-Job -FilePath "$scriptPath\createReminder.ps1" -ArgumentList $screenIndex, "$textToDisplay"
-    $jobList.Add($job)
-}
-
-foreach($job in $jobList)
-{
-    Wait-Job $job
+    Start-Process -WindowStyle Minimized -FilePath "powershell" -ArgumentList "-file $scriptPath\createReminder.ps1 $screenIndex ""$textToDisplay"""
 }
